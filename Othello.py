c75d5a28915523d2d0b1533c9edad85ab9b2fe1c
Luke256 <yuta.k.200628@gmail.com>
2020-10-08 19:47:46 +0900

    Make Othello System

--- Othello.py
+++ Othello.py
@@ -26,7 +26,11 @@ class Othello:
         ok=self.isOK(act)
         if ok:
             self.isOK(act,True)
-            print(self.field)
+            for i in self.field:
+                print(i)
+            self.turn=3-self.turn
+        else:
+            print("You can't put on {}".format(act))
 
         f=True
         for i in self.field:
@@ -34,41 +38,54 @@ class Othello:
                 if j==0:
                     f=False
         self.finish=f
+        
+        return
 
     def isOK(self,act,shift=False):
         vec=[-1,0,1]
-        ok=True
+        ok=False
+        
+        if(self.field[act//8][act%8]!=0):
+            return False
 
         for i in vec:
             for j in vec:
                 if i==0 and j==0:
                     continue
-                if(not self.isOK_sub(act,i,j,shift)):
-                    ok=False
+                if(self.isOK_sub(act,i,j,shift)):
+                    ok=True
 
         return ok
 
-    def isOK_sub(self,act,i,j,shift):
-        n_i=act/8
+    def isOK_sub(self,act,i,j,shift,req=False):
+        n_i=act//8
         n_j=act%8
         next=(n_i+i)*8+n_j+j
 
         if not (0<=n_i+i<8 and 0<=n_j+j<8):
             return False
 
-        if self.field[n_i+i][n_j+j]==self.turn:
-            result=self.isOK_sub(next,i,j)
-            if result:
-                self.field[n_i+i][n_j+j]=3-self.field[n_i+i][n_j+j]
+        if self.field[n_i+i][n_j+j]==3-self.turn:
+            result=self.isOK_sub(next,i,j,shift,req=True)
+            if result and shift:
+                self.field[n_i][n_j]=self.turn
             return result
         elif self.field[n_i+i][n_j+j]==0:
             return False
         else:
-            self.field[n_i+i][n_j+j]=3-self.field[n_i+i][n_j+j]
-            return True
+            if req:
+                if shift:
+                    self.field[n_i][n_j]=self.turn
+                return True
+            else:
+                return False
 
 
 if __name__=="__main__":
     a=Othello()
-    while(a.finish==False):
-        a.step(random.randrange(0,81))
+    for i in a.field:
+        print(i)
+        
+    for i in range(64):
+        a.step(i)
+        print()
